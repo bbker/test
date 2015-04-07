a96c1562a71cd1e301e264be950880f5a5a5d028
bbker <q769880475@163.com>
2015-04-07 23:41:56 +0800

    label

---
+++ Form1(helloword label move).cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+namespace WindowsFormsApplication3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+             InitializeComponent();
+        }
+
+        private Point movePoint;
+        private void label1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                movePoint = e.Location;
+            }
+        }
+
+        private void label1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                label1.Location = new Point(label1.Location.X + e.X - movePoint.X, label1.Location.Y + e.Y - movePoint.Y);
+            }
+        }
+
+
+    }
+}
